5a73e54fc06607ff973b6d42c9932cb61f47a399
Harley Swift <=>
2022-05-01 20:21:57 +0100

    Buttons are now working

--- src/components/Section1/Section1.js
+++ src/components/Section1/Section1.js
@@ -1,14 +1,21 @@
 import "./Section1.css";
 import pc_icon from "../../assets/images/pc.png";
 import Button from "../Utils/Button/Button";
+import {useNavigate} from "react-router";
 
 export default function Section1() {
+    const navigate = useNavigate();
+
+    const handleClick = function() {
+        navigate("/contact")
+    }
+
     return(<section id={"section1"} className={"section1"}>
         <div className="section1-content">
             <div className="section1-content-left">
                 <h1>My <blue>Services</blue></h1>
                 <p>I offer various types of custom-made websites including blogs, business and personal websites, personalised for you.</p>
-                <Button text={"Get A Quote"} />
+                <Button onClick={handleClick} text={"Get A Quote"} />
             </div>
             <img src={pc_icon} alt=""/>
         </div>
--- src/components/Section4/PricingWidget/PricingWidget.js
+++ src/components/Section4/PricingWidget/PricingWidget.js
@@ -1,11 +1,18 @@
 import "./PricingWidget.css";
 import Button from "../../Utils/Button/Button";
+import {useNavigate} from "react-router";
 
 export default function PricingWidget(props) {
+    const navigate = useNavigate();
+
     const title = props.title;
     const description = props.description;
     const price = props.price;
     const items = props.items;
+
+    const handleClick = function() {
+        navigate("/contact")
+    }
 
     return (<article className="pricing-widget">
         <h1>{title}</h1>
@@ -17,6 +24,6 @@ export default function PricingWidget(props) {
                 return <li key={index}><b>{item}</b></li>
             })}
         </ul>
-        <Button width={"18rem"} text={"Customize Order"} />
+        <Button onClick={handleClick} width={"18rem"} text={"Customize Order"} />
     </article>)
 }
