11add9f8d3ce756312b83e0cf168192a94e89a3f
Harley Swift <=>
2022-05-01 19:28:51 +0100

    Fixed some responsive-based bugs

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,13 +4,17 @@ import cloud from "../../assets/images/cloud.png";
 import Button from "../Utils/Button/Button";
 
 export default function Hero() {
+
+    const handleClick = function() {
+        alert("Clicked");
+    }
     return (<section className={"hero"}>
         <div className="hero-content">
             <img src={hero_image} className={"hero-image"} alt=""/>
             <img src={cloud} className={"cloud-1"} alt=""/>
             <img src={cloud} className={"cloud-2"} alt=""/>
             <h1>The <blue>ultimate</blue> website solution for your business</h1>
-            <Button text={"Learn More"} />
+            <Button onClick={handleClick} text={"Learn More"} />
         </div>
     </section>)
 }
--- src/components/Utils/Button/Button.js
+++ src/components/Utils/Button/Button.js
@@ -2,7 +2,7 @@ import "./Button.css";
 
 export default function Button(props) {
     const text = props.text;
-    return(<button style={{width: props.width}}>
+    return(<button onClick={props.onClick} style={{width: props.width}}>
         {text}
     </button>)
 }
